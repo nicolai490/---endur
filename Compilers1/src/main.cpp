a4396c96a8569d6ae31d71e09ac83aaffa668678
Nicolai <nicolai13@ru.is>
2013-09-12 14:22:45 +0000

    Fruitless attempt at figuring out the flex problem.

--- Compilers1/src/main.cpp
+++ Compilers1/src/main.cpp
@@ -2,7 +2,10 @@
 #include "symtab.h"
 #include "token.h"
 #include "scanner.h"
+#include "lex.yy.cc"
 
 int main(){
+    FlexLexer* m_lexer = new yyFlexLexer();
+    m_lexer->yylex();
     return 0;
 }
